83c5ffbdda35babe348c5a7af9458856da378267
Anandakii <anandaokky38@gmail.com>
2021-11-30 22:27:54 +0700

    Pertemuan 11

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,9 +13,9 @@ class Admin extends CI_Controller
     {
         $data['judul'] = 'Dashboard';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-$data['anggota']= $this->ModelUser->getUserLimit()->result_array();
- $data['buku']= $this->ModelBuku->getBuku()->result_array();
- $this->load->view('templates/header', $data);
+        $data['anggota']= $this->ModelUser->getUserLimit()->result_array();
+        $data['buku']= $this->ModelBuku->getBuku()->result_array();
+        $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
         $this->load->view('admin/index', $data);
